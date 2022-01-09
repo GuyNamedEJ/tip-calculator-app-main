1f70a21bc01d0aba0f3281807c6b0f108cff9fd8
EJ Milord <jmilord7@gmail.com>
2022-01-09 18:47:38 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Define variables here
+
+
+// Tip Buttons
+/* 
+    1. Add onclick events to all the buttons
+    2. Create switch statement
+        - ex. if button.value = 5% tip = 0.05
+*/
+
+// Calculate the tip per person
+
+
+
+
+//Calculate the total amount per person
+
+
+//Reset Button - onClick, clear all fields
