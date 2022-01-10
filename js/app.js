03a9473d2c47e4392f8ca30f88e34a3775423bed
EJ Milord <jmilord7@gmail.com>
2022-01-10 15:28:29 -0500

    Calculator logic completed

--- js/app.js
+++ js/app.js
@@ -1,19 +1,101 @@
 // Define variables here
+let tipButtons = document.getElementsByClassName("tip-amount");
+let billAmount = document.getElementById("bill");
+let numPeople = document.getElementById("people");
+let tipPerPersonDisplay = document.getElementById("tip-per-person");
+let totalPerPersonDisplay = document.getElementById("per-person-cost");
+let errorMessage = document.getElementById("error");
+let tip;
+let tipPerPerson;
+let customTip = document.getElementById("custom-tip");
+
+for (let i = 0; i < tipButtons.length; i++) {
+  tipButtons[i].addEventListener("click", setTip);
+}
+
+function setCustomTip()
+{
+    
+}
+
+function setTip() {
+    let tipButton = this;
+
+    if(!isNaN(customTip.value))
+    {
+        tip = customTip.value / 100;
+
+        for(let i = 0; i < tipButtons.length; i++)
+        {
+            tipButtons[i].classList.remove("active");
+        }
+
+        calcGratuity();
+
+    }
+    for (let i = 0; i < tipButtons.length; i++) 
+    {
+      if(tipButton.value === tipButtons[i].value)
+      {
+          tipButton.classList.add("active");
+          tip = tipButton.value / 100;
+          calcGratuity();
+      }
+      else
+      {
+          tipButtons[i].classList.remove("active");
+      }
+    }
+  }
+
+billAmount.addEventListener("input",calcGratuity);
+numPeople.addEventListener("input",calcGratuity);
+customTip.addEventListener("input",setTip);
+function calcTipPerPerson()
+{
+    tipPerPerson;
+    let calcTip;
+    calcTip = billAmount.value * tip / numPeople.value;
+    if(isNaN(calcTip) || calcTip == Infinity)
+        {
+            tipPerPerson = 0.00;
+        }
+        else
+        {
+            tipPerPerson = calcTip;
+        }
+    
+    return tipPerPerson.toFixed(2);
+}
+
+function calcTotalPerPerson()
+{
+    let totalPerPerson;
+    let calcTotal;
+    
+
+    calcTotal = (billAmount.value / numPeople.value) + tipPerPerson;
+
+    if(isNaN(calcTotal) || calcTotal == Infinity)
+    {
+        totalPerPerson = 0.00;
+    }
+    else
+    {
+        totalPerPerson = calcTotal;
+    }
+    console.log(totalPerPerson);
+    return totalPerPerson.toFixed(2);
+}
 
 
-// Tip Buttons
-/* 
-    1. Add onclick events to all the buttons
-    2. Create switch statement
-        - ex. if button.value = 5% tip = 0.05
-*/
-
-// Calculate the tip per person
+function calcGratuity()
+{
+    tipPerPersonDisplay.innerHTML = calcTipPerPerson();
+    totalPerPersonDisplay.innerHTML = calcTotalPerPerson();
+}
 
 
 
 
-//Calculate the total amount per person
 
-
-//Reset Button - onClick, clear all fields
